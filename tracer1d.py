9342ccb5d4fe1d07834e47e4e136d2e36130d1bf
Derek Steinmoeller <dsteinmo@gmail.com>
2023-02-05 13:24:29 -0500

    Formatting/linting for tracer1d.py.

--- tracer1d.py
+++ tracer1d.py
@@ -2,6 +2,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 
+
+# Define upwind flux operation given fields at - (uM) and + (uP)
+# cell edges and a constant 'wind' speed, c and direction sign(c).
+def upwind_flux_1d(uM, uP, normal, c, Nx, abs_tol=1e-15):
+    flux = np.zeros((Nx,))
+    normal_velocity = normal * c
+
+    msk = normal_velocity >= abs_tol
+    flux[msk] = uM[msk]
+
+    msk = normal_velocity < -abs_tol
+    flux[msk] = uP[msk]
+
+    return flux
+
+
+# Define central flux operation given fields at - (uM) and + (uP)
+# cell edges.
+def central_flux_1d(uM, uP, normal, c, Nx):
+    flux = np.zeros((Nx,))
+    flux = 0.5*(uM + uP)
+    return flux
+
+
 Nx = 128
 Lx = 3000
 
@@ -26,31 +50,12 @@ NUMSTEPS = 2000
 NUMOUTS = 50
 OUT_INTERVAL = int(NUMSTEPS/NUMOUTS)
 
-
-def upwind_flux_1d(uM, uP, normal, c, Nx, abs_tol=1e-15):
-    flux = np.zeros((Nx,))
-    normal_velocity = normal * c
-
-    msk = normal_velocity >= abs_tol
-    flux[msk] = uM[msk]
-    
-    msk = normal_velocity < -abs_tol
-    flux[msk] = uP[msk]
-    
-    return flux
-
-def central_flux_1d(uM, uP, normal, c, Nx):
-    flux = np.zeros((Nx,))
-    flux = 0.5*(uM + uP)
-    return flux
-
-
 plt.figure()
 t = 0.0
 output_times = []
 for n in range(0, NUMSTEPS):
     cT = c*T
-    Tin  = np.cos(2*np.pi*(-0.5*dx-c*t)/Lx)
+    Tin = np.cos(2*np.pi*(-0.5*dx-c*t)/Lx)
     cTL = np.concatenate([[c*Tin], cT[:-1]])
     cTR = np.concatenate([cT[1:], [c*T[-1]]])
 
@@ -66,11 +71,10 @@ for n in range(0, NUMSTEPS):
         Fjph[-1] = cTR[-1]
         Fjmh[-1] = cT[-1]
 
-
     T -= (dt/dx)*(Fjph - Fjmh)
     t += dt
-    
-    if n % OUT_INTERVAL == 0:        
+
+    if n % OUT_INTERVAL == 0:
         plt.plot(x, T)
         plt.ylim([-1.005, 1.005])
         plt.ylabel('$\overline{T}$')
@@ -83,13 +87,12 @@ for n in range(0, NUMSTEPS):
         output_times.append(int(t))
         if len(output_times) == 6:
             legend_entries = (f"t={output_times[0]}", f"t={output_times[1]}",
-                f"t={output_times[2]}", f"t={output_times[3]}",
-                f"t={output_times[4]}", f"t={output_times[5]}")
+                              f"t={output_times[2]}", f"t={output_times[3]}",
+                              f"t={output_times[4]}", f"t={output_times[5]}")
 
             plt.legend(legend_entries)
             with PdfPages("1d_upwinding_cosine.pdf") as export_pdf:
                 export_pdf.savefig()
             exit()
-                
-        plt.pause(0.5)
 
+        plt.pause(0.5)
